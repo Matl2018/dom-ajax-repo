ebb7c583b189cdf6e3911c4b0d19ae0461af6a9e
matl2018 <lumierematerne11@gmail.com>
2018-04-23 14:09:53 +0100

    Part 1 ( task 1-3 ) completed

--- js/my-github.js
+++ js/my-github.js
@@ -1,2 +1,21 @@
 // Write code here to communicate with Github
+//AJAX call
+fetch("https://api.github.com/users/matl2018/repos")
+  .then(function(data) {
+    return data.json();
+  })
+  .then(function(repos) {
+    var reposList = document.getElementById("repos-list");
+    var reposCount = document.querySelector("#repos-count");
+    reposCount.innerHTML = repos.length;
 
+    repos.forEach(function(repo) {
+      var listItem = document.createElement("li");
+      var itemLink = document.createElement("a");
+      itemLink.setAttribute("href", repo.html_url);
+      itemLink.innerHTML = repo.name;
+
+      listItem.appendChild(itemLink);
+      reposList.appendChild(listItem);
+    });
+  });
